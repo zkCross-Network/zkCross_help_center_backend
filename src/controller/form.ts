a4c2d4f79cc214aa1c0c3543cd279247f1da4cfd
nishant-7049 <nishantsharmay2@gmail.com>
2025-01-23 16:21:11 +0530

    not taking address as mandatory

--- src/controller/form.ts
+++ src/controller/form.ts
@@ -1,28 +1,45 @@
-import { Request,Response,NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import { emailTemplate } from "../helper/template/email";
-import {sanitizeString} from "../helper/index";
+import { sanitizeString } from "../helper/index";
 import Service from "../service/nodemailer";
 import sendMessageToChannel from "../service/sendTelegramMessage";
 
-export const formSubmit = async (req:Request,res:Response,next:NextFunction)=>{
-    try{
-        const {email,product,blockchain,address,message} = req.body;
-        if(!(sanitizeString(email) && sanitizeString(product) && sanitizeString(blockchain) && sanitizeString(address) && sanitizeString(message))){
-            return res.status(400).json({success:false,message:"missing mandatory fields"});
-        }
-        const emailText = emailTemplate({...req.body});
-        await Service.sendMail(emailText);
-        if(blockchain !== "Partisia"){
-            return res.status(200).json({success: true,message:"email sent successfully" })
-        }
-        let telegramMessage = 'Help Center Message\n — — — — — — — — — — — \n'
-        for(let field in req.body){
-            telegramMessage+=`${field}: ${req.body[field]}\n`
-        }
-        sendMessageToChannel(telegramMessage)
-        res.status(200).json({success:true,message:"email and telegram message sent successfully"});
+export const formSubmit = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, product, blockchain, address, message } = req.body;
+    if (
+      !(
+        sanitizeString(email) &&
+        sanitizeString(product) &&
+        sanitizeString(blockchain) &&
+        sanitizeString(message)
+      )
+    ) {
+      return res
+        .status(400)
+        .json({ success: false, message: "missing mandatory fields" });
     }
-    catch(err:any){
-        res.status(500).json({success:false,message:err.message})
+    const emailText = emailTemplate({ ...req.body });
+    await Service.sendMail(emailText);
+    if (blockchain !== "Partisia") {
+      return res
+        .status(200)
+        .json({ success: true, message: "email sent successfully" });
     }
-}
+    let telegramMessage = "Help Center Message\n — — — — — — — — — — — \n";
+    for (let field in req.body) {
+      telegramMessage += `${field}: ${req.body[field]}\n`;
+    }
+    sendMessageToChannel(telegramMessage);
+    res.status(200).json({
+      success: true,
+      message: "email and telegram message sent successfully",
+    });
+  } catch (err: any) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
